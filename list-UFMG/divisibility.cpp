adb085fdb55bda0269e79df1550f4288c7550391
Letty <lettinwonderland@gmail.com>
2024-03-02 18:57:30 -0300

    exercicio divisibility

--- list-UFMG/divisibility.cpp
+++ list-UFMG/divisibility.cpp
@@ -7,5 +7,20 @@ using namespace std;
 
 int main(){
 
+    int n;
+    cin >> n;
+    int a=0, b=0;
+
+    for(int i=0; i < n; i++){
+        cin >> a >> b;
+
+        if(a%b == 0){
+            cout << "0" << endl;
+        } 
+        else{
+            cout << b - (a % b) << endl;
+        }
+    }
+
     return 0;
 }
